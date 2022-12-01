804080c84bd275e4a23b12a1cf7b8a16bc77720e
Emmanuel Yeboah <noelzappy@gmail.com>
2022-12-01 23:58:49 +0000

    auth-module: Added authentication by username route

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -7,12 +7,21 @@ const auth = require('../../middlewares/auth');
 const router = express.Router();
 
 router.post('/register', validate(authValidation.register), authController.register);
+
 router.post('/login', validate(authValidation.login), authController.login);
+
+router.post('/login-with-username', validate(authValidation.loginWithUsername), authController.loginWithUsername);
+
 router.post('/logout', validate(authValidation.logout), authController.logout);
+
 router.post('/refresh-tokens', validate(authValidation.refreshTokens), authController.refreshTokens);
+
 router.post('/forgot-password', validate(authValidation.forgotPassword), authController.forgotPassword);
+
 router.post('/reset-password', validate(authValidation.resetPassword), authController.resetPassword);
+
 router.post('/send-verification-email', auth(), authController.sendVerificationEmail);
+
 router.post('/verify-email', validate(authValidation.verifyEmail), authController.verifyEmail);
 
 module.exports = router;
