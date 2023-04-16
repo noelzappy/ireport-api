1811608964edd8081ccbd66d521a3bf441d36163
Emmanuel Yeboah <noelzappy@gmail.com>
2023-04-16 19:25:26 +0000

    feat: added db migrations

--- src/interfaces/users.interface.ts
+++ src/interfaces/users.interface.ts
@@ -2,4 +2,13 @@ export interface User {
   id?: number;
   email: string;
   password: string;
+  name: string;
+  first_name: string;
+  last_name: string;
+  phone?: string;
+  id_number?: string;
+  id_type?: string;
+  is_verified?: boolean;
+  is_email_verified?: boolean;
+  username?: string;
 }
--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -1,13 +1,21 @@
 import { Sequelize, DataTypes, Model, Optional } from 'sequelize';
 import { User } from '@interfaces/users.interface';
 
-export type UserCreationAttributes = Optional<User, 'id' | 'email' | 'password'>;
+export type UserCreationAttributes = Optional<User, 'id'>;
 
 export class UserModel extends Model<User, UserCreationAttributes> implements User {
-  public id: number;
+  declare id: number;
   public email: string;
   public password: string;
   public name: string;
+  public first_name: string;
+  public last_name: string;
+  public phone: string;
+  public id_number: string;
+  public id_type: string;
+  public is_verified: boolean;
+  public is_email_verified: boolean;
+  public username: string;
 
   public readonly createdAt!: Date;
   public readonly updatedAt!: Date;
@@ -28,6 +36,44 @@ export default function (sequelize: Sequelize): typeof UserModel {
       password: {
         allowNull: false,
         type: DataTypes.STRING(255),
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING(45),
+      },
+      first_name: {
+        allowNull: true,
+        type: DataTypes.STRING(45),
+      },
+      last_name: {
+        allowNull: true,
+        type: DataTypes.STRING(45),
+      },
+      phone: {
+        allowNull: true,
+        type: DataTypes.STRING(45),
+      },
+      id_number: {
+        allowNull: true,
+        type: DataTypes.STRING(45),
+      },
+      id_type: {
+        allowNull: true,
+        type: DataTypes.STRING(45),
+      },
+      is_verified: {
+        allowNull: true,
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      is_email_verified: {
+        allowNull: false,
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      username: {
+        allowNull: true,
+        type: DataTypes.STRING(45),
       },
     },
     {
